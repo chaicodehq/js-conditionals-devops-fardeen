bc0f3a8c53da37cedb231770cfa4420989221d19
Mohd Fardeen <mail.mohdfardeen@gmail.com>
2026-02-24 23:02:18 +0530

    Complete sixth test

--- src/06-shipping-calculator.js
+++ src/06-shipping-calculator.js
@@ -29,5 +29,52 @@
  * @returns {number} Shipping cost, 0 for free shipping, or -1 for invalid input
  */
 export function calculateShipping(weight, country, orderTotal) {
-  // Your code here
+  if (weight <= 0 || orderTotal < 0 ) return -1
+
+  // Free Shipping:
+if (country === "US" && orderTotal > 50 )
+{
+  return 0;
 }
+if (country !== "US" && orderTotal > 100)
+{
+  return 0;
+}
+
+// Domestic Shipping (country === "US"):
+if (country === "US")
+{
+  if (weight <= 1)
+  {
+    return 5;
+  }
+  if (weight <= 5)
+  {
+    return 10;
+  }
+  if (weight > 5)
+  {
+    return 15;
+  }
+}
+
+
+// International Shipping (any other country):
+if (country !== "US")
+{
+  if (weight <= 1)
+  {
+    return 15;
+  }
+  if (weight <= 5)
+  {
+    return 25;
+  }
+  if (weight > 5)
+  {
+    return 40;
+  }
+}
+
+}
+
